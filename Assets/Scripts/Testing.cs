7e8e00faaa1bf5da99e9a62918d5ba5b488c7e3e
Humusk <gguixe23@gmail.com>
2022-06-16 23:35:27 +0200

    Creating a better grid (+simple, +clear)

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+//TUTORIAL HELP: https://www.youtube.com/watch?v=waEsGu--9P8&t=111s
+
+
+public class Testing : MonoBehaviour
+{
+
+    private Grid grid;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        grid = new Grid(4, 2, 10f); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+    }
+
+}
