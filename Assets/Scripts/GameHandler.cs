95b029076eb3c0d72b2410b64c25202d511f05b2
gguixe <gguixe23@gmail.com>
2022-06-22 23:36:24 +0200

    start of combat grid (tengo sueño)

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -10,22 +10,42 @@ public class GameHandler : MonoBehaviour
     public static GameHandler Instance { get; private set; }
 
     [SerializeField] private Transform cinemachineFollowTransform;
-    //[SerializedField] private MovementTilemapVisual movementTilemapVisual;
+    [SerializeField] private TilemapVisual tilemapVisual;
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
-    //private Grid<EmptyGridObject> grid;
-    //private MovementTilemap movementTilemap;
+    private Grid<EmptyGridObject> grid;
+    private Tilemap tilemap;
     public Pathfinding pathfinding;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
+        Instance = this;
 
-    // Update is called once per frame
-    void Update()
+        int mapWidth = 40;
+        int mapHeight = 25;
+        float cellSize = 10f;
+        Vector3 origin = new Vector3(0, 0);
+
+        grid = new Grid<EmptyGridObject>(mapWidth, mapHeight, cellSize, origin, (Grid<EmptyGridObject> g, int x, int y) => new TilemapObject(g, x, y));
+
+        pathfinding = new Pathfinding(origin + new Vector3(1, 1) * cellSize * .5f, new Vector3(mapWidth, mapHeight));
+        //pathfinding.RaycastWalkable();
+
+        tilemap = new Tilemap(mapWidth, mapHeight, cellSize, origin);
+    }
+}
+
+//EMPTY GAME OBJECT
+public class EmptyGridObject
+{
+    private Grid<EmptyGridObject> grid;
+    private int x;
+    private int y;
+
+    public EmptyGridObject(Grid<EmptyGridObject> grid, int x, int y)
     {
-        
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
     }
 }
